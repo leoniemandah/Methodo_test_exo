d7c90d2bdc90c9cbc5165f5c40cf85b507cfa3f0
Leonie Mandah <andjibyleonieexode.mandah@ynov.com>
2024-01-21 17:04:42 +0100

    suppression fichier

--- App/Domaine/Temps/Matin.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace User\MtExeciceKata1\App\Domaine\Temps;
-class Matin extends MomentDeLaJournee {
-    public function __construct (){
-        parent::__construct(parent::MATIN);
-    }
-
-}
